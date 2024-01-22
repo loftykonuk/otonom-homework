f38d3f08953dfe73eabcc3aa4c2edd11e697ecea
loftykonuk <alperenkonuk93@gmail.com>
2024-01-22 17:39:38 +0300

    commit2

--- client.py
+++ client.py
@@ -3,19 +3,26 @@ import struct
 import time
 
 
+def send(data1, data2, data3):
+    client_socket1.sendall(data1)
+    client_socket1.sendall(data2)
+    client_socket1.sendall(data3)
+
+
 host = '34.234.82.161'
 port = 13571
-
-client_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket1.connect((host, port))
 
 cn = "lofty"
 cn = cn.ljust(32)
 tn = "topic konuk"
 tn = tn.ljust(32)
 a = 1000
-message1 = cn.encode('utf-8') + tn.encode('utf-8') + struct.pack('!I', a)
+data3 = struct.pack('>I', a)
+
+
+client_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket1.connect((host, port))
 
 while True:
-    client_socket1.sendall(message1)
+    send(cn, tn, data3)
     time.sleep(5)
